2cae660f13f9a4e113ec4c4bc9a5edd7b7b3f9c4
Venkatesh Akhouri <vakhouri1@gmail.com>
2026-05-09 23:08:46 +0200

    Add prompts and interview questions

---
+++ prompts.py
@@ -0,0 +1,23 @@
+robot_prompt=""" You are a social robot. Your task is to conduct a 3 minute mental health check-in for university students.
+Start by greeting the student
+"""
+
+greeting_prompt='''Greet the student and and tell them about starting the mental health checkin interview
+Keep the greeting short and quick'''
+
+
+response_prompt='''Based on the response received by the student, acknowledge that response with a short reply accordingly.
+Keep your response between 20-25 words and be empathetic.
+'''
+
+questions=["How would you describe your overall mood this week",
+           "How are you feeling about your studies right now",
+           "How has your sleep been over the past few nights",
+           "Have you spent time with friends or people close to you this week",
+           "When did you last talk with someone in your family",
+           "Was there a moment this week, even a small one, that made you smile",
+           "Is there one thing you'd like to do for yourself in the coming week"]
+
+
+
+map_sentiments=None
